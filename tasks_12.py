286c3b1bd6f2afe1538f10110c5f513496e2aa72
MaximSeluzhitckii <166321133+MaximSeluzhitckii@users.noreply.github.com>
2024-04-21 19:27:42 +0300

    Update tasks_12.py

--- tasks_12.py
+++ tasks_12.py
@@ -7,4 +7,19 @@
 Обратите внимание, что пустая строка также является 
 правильной скобочной последовательностью.
 """
-print('first pull request')
+def chek_string_brakets(input_string):
+    spisok = []
+    for char in input_string:
+        if char == '(':
+            spisok.append(char)
+        elif char == ')':
+            if len(spisok) == 0:
+                print('NO')
+                return
+            spisok.pop()
+    if len(spisok) == 0:
+        print('YES')
+    else:
+        print('NO')
+
+chek_string_brakets('()')
