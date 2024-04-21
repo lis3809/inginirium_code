f6364d1ca83bdeca5e750579368a08571405692d
peper1234 <167698712+peper1234@users.noreply.github.com>
2024-04-21 19:28:19 +0300

    i completed task

--- tasks_12.py
+++ tasks_12.py
@@ -7,4 +7,21 @@
 Обратите внимание, что пустая строка также является 
 правильной скобочной последовательностью.
 """
-print('first pull request')
+def check_string_brackets(input_string):
+    spis = []
+    for char in input_string:
+        if char == '(':
+            spis.append(char)
+        elif char == ')':
+            if len(spis) == 0:
+                print('No')
+                return
+            spis.pop()
+    if len(spis) == 0:
+        print('Yes')
+    else:
+        print('No')
+
+
+check_string_brackets('()')
+
